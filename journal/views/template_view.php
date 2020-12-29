7a25242576a7f784f2ca4530c1474fc6989d980c
henri <llc-izograd@yandex.ru>
2020-12-29 16:30:28 +0300

    1
    first commit

---
+++ journal/views/template_view.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="ru">
+<link href="style.css" rel="stylesheet" type="text/css">
+<head>
+ <meta charset="utf-8">
+ <title>Главная</title>
+</head>
+<body>
+ <?php include 'views/'.$content_view; ?>
+</body>
+</html> 
